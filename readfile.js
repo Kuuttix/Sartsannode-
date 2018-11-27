01cc37f00d1438137afb30497d0dfcc859624c6e
Kuuttix <sara.harvonen@edu.turkuamk.fi>
2018-11-27 07:00:04 -0800

    Add files via upload

---
+++ readfile.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+
+fs.readfile('runo.txt',function(err.data) {
+	if (err) return console.error(err);
+	console.log(data.toString());
+});
+
+console.log("ohjelma on suoritettu loppuun");
+
+
